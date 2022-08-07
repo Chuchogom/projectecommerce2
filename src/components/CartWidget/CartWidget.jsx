67fe0dad15d451cc575330580a17c9e976ba5b53
Jesusgomrec <90487411+Chuchogom@users.noreply.github.com>
2022-08-07 12:30:29 -0500

    Sincronizacion de counter

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -5,14 +5,14 @@ import './CartWidget.css'
 
 const CartWidget = () => {
     const [toggleOrders, setToggleOrders] = useState(false);
-    // const {state:{cart}}=useContext(AppContext);
+    const {state:{cart}}=useContext(AppContext);
   return (
       <div
           className="navbar-shopping-cart"
           onClick={() => setToggleOrders(!toggleOrders)}
       >
           <img src={shoppingCart} alt="shopping cart" />
-          {/* {cart.length > 0 ? <div>{cart.length}</div> : null} */}
+          {cart.length > 0 ? <div>{cart.length}</div> : null}
 
       </div>
   )
